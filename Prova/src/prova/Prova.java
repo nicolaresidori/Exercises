4f796f4b674092fc63aebe2f0ccb1e0bb86f6a1a
Softuring <nicola_residori@icloud.com>
2015-06-17 09:44:50 +0200

    Nuovo Progetto di Nome Prova

---
+++ Prova/src/prova/Prova.java
@@ -0,0 +1,14 @@
+/* Created on 17-giu-2015 */
+package prova;
+
+/**
+ *
+ * @author Nicola Residori (nicola_residori@icloud.com)
+ */
+public class Prova {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
